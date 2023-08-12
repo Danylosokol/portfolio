d476daa01fed710e15e3ca51ec6484e39bc903c3
dsoSU <danylo.sokol2504@gmail.com>
2023-08-12 19:08:05 +0100

    fix deploy

--- app/(site)/components/main/Projects.tsx
+++ app/(site)/components/main/Projects.tsx
@@ -9,12 +9,12 @@ export default async function Projects() {
     <section className="mt-32 mb-20">
       <div className="mb-16">
         <h2 className="font-semibold text-4xl">
-          Featured projects I've worked on
+          Featured projects I&apos;ve worked on
         </h2>
       </div>
       <section className="grid md:grid-cols-3 grid-cols-1 gap-32 mb-12">
-        {projects.map((project) => (
-          <Project project={project} />
+        {projects.map((project, indx) => (
+          <Project key={`project-${indx}`} project={project} />
         ))}
       </section>
     </section>
--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -10,7 +10,7 @@ export default async function Home() {
   return (
     <main className="max-w-7xl mx-auto lg:px-16 px-6">
       <section className="flex xl:flex-row flex-col xl:items-center items-start xl:justify-center justify-between gap-x-12 lg:mt-32 mt-20 mb-16">
-        {profile && profile.map((data) => <Hero data={data} />)}
+        {profile && profile.map((data, indx) => <Hero key={`hero-${indx}`} data={data} />)}
       </section>
       <div id="expertise" className="scroll-m-10"></div>
       <Jobs skills={profile[0].skills} />
